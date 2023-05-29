0cf7cd66c8ddd735d54cfd2f4375f01cebdfc9bf
Arthur Mousinho <arthursilvamo296@gmail.com>
2023-05-29 09:31:01 -0300

    fixing error message

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const main = document.querySelector('main')
 
 button.addEventListener('click', async () => {
     if (Validation.inputIsEmpty()) {
-        alert('You need to enter you github username to keep going')
+        alert('You need to enter your github username to keep going')
     } else {
         const username = document.querySelector('#github-username-input').value
         const userObject = await getUser(username)
